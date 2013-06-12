7c1316dff3a079c02438276fb634e7e1cc8a8c0f
Rune Lind <rulind@gmail.com>
2013-06-12 16:12:23 +0200

    added params to search based on live queries

--- lib/solr_performance_testing.rb
+++ lib/solr_performance_testing.rb
@@ -3,8 +3,11 @@ require "solr"
 require 'rsolr'
 require "json"
 require "benchmark"
+require_relative '../lib/logging'
 
 class SolrPerformanceTesting
+  include Logging
+
   def companies
     @companies ||= open(File.dirname(__FILE__) + '/../data/search_queries.txt', "r:UTF-8").lines.map { |line| line.strip }
   end
@@ -27,8 +30,8 @@ class SolrPerformanceTesting
   end
 
   def search_companies(max_count=1, thread_cnt=1, serves = {
-        'n29' => 'http://datanode29.companybook.no:8360/solr/gb_companies_20130418',
-    })
+      'n29' => 'http://datanode29.companybook.no:8360/solr/gb_companies_20130418',
+  })
 
     puts "will search #{max_count} companies"
 
@@ -56,16 +59,56 @@ class SolrPerformanceTesting
       q_times.sort_by { |name, value| name }.each { |k, v| puts "#{k} - #{v}" }
       slowest_counts.sort_by { |name, value| name }.each { |k, v| puts "#{k} - #{v}" }
       puts "Total Qtime used:" + q_times.values.inject { |sum, v| sum + v }.to_s
-      puts "Total Worst used:" + slowest_counts.values.map { |v| v[0] }. inject { |sum, v| sum + v }.to_s
+      puts "Total Worst used:" + slowest_counts.values.map { |v| v[0] }.inject { |sum, v| sum + v }.to_s
     end
-
   end
 
   def search(url, search_text="*:*")
     #puts search_text
     solr = RSolr.connect :url => url
-    response = solr.get 'select', :params => {q: search_text, rows:1, fl:'company_id'}
-    return response['responseHeader']['QTime'].to_i, response['response']['numFound'].to_i
+
+    params = {
+        :q => search_text,
+        #:facet => 'true',
+        :utf8 => '?',
+        :location => '150-154-GB',
+        'shards.qt' => 'dismax_flat2',
+        'facet.limit' => '3',
+        'f.location.facet.limit' => '10',
+        :hl => 'true',
+        :version => '2',
+        :fl => 'name,company_id,revenue,org_num,city,geolocation,geolocation_source,country_iso,industry_code,nace_2,naics_4,naics_6,employees_count,structure,profit,location,region,sub_region',
+        :bq => 'country_iso:GB^500+company_id:GB0000000288451024^9999',
+        'facet.query' => ['employees_count:[1+TO+10]', 'employees_count:[11+TO+50]', 'employees_count:[51+TO+200]', 'employees_count:[201+TO+500]', 'employees_count:[501+TO+1000]', 'employees_count:[1001+TO+5000]', 'employees_count:[5001+TO+10000]', 'employees_count:[10001+TO+10000000]', 'revenue:[0.02+TO+500000]', 'revenue:[0.05+TO+500000]', 'revenue:[1+TO+500000]', 'revenue:[10+TO+500000]', 'revenue:[80+TO+500000]', 'revenue:[200+TO+500000]', 'revenue:[500+TO+500000]', 'profit:[-2100+TO+-0.0001]', 'profit:[0.02+TO+500000]', 'profit:[0.05+TO+500000]', 'profit:[1+TO+500000]', 'profit:[10+TO+500000]', 'profit:[80+TO+500000]', 'profit:[200+TO+500000]', 'profit:[500+TO+500000]'],
+        :bf => 'log(max(1,boost_revenue))^200',
+        :action => 'show',
+        'facet.field' => ['location', 'region', 'naics_6'],
+        :qt => 'dismax_flat',
+        'f.naics_6.facet.limit' => '30',
+        'hl.fragsize' => '280',
+        :industry_classification => 'naics_6',
+        :industry_coverage => '0.6',
+        'facet.mincount' => '1',
+        :resource => 'search',
+        'hl.fl' => ['body', 'body_1', 'body_2', 'name', 'title', 'keywords', 'meta_description', 'ceo', 'chairman', 'board_member', 'key_people'],
+        'hl.maxAnalyzedChars' => '500000',
+        'f.region.facet.limit' => '5',
+        :rows => '15',
+        :user_country_iso => 'GB',
+        :controller => 'companies/search',
+        'hl.usePhraseHighlighter' => 'true',
+        :pf => 'title^10 meta_description^10 keywords^10 name^10',
+        :qf => 'body_1^0.05 body^0.1 body_2^0.01 chairman structure^0.1 keywords^0.3 ext_link_texts^0.5 city^4 key_people ceo title^0.3 meta_description^0.3 name^10 board_member',
+        :fq => ['location:150-154-GB', '-active:1', '-region:039', 'naics_6:551112 OR name:"holding"'],
+    }
+
+    #response = solr.get 'select', :params => {q: search_text, rows:1, fl:'company_id', facet: true, 'shards.qt' => 'dismax_flat'}
+    response = solr.get 'select', :params => params
+
+    query_time = response['responseHeader']['QTime'].to_i
+    hits = response['response']['numFound'].to_i
+    log.info "#{search_text} qTime:#{query_time} hits:#{hits}"
+    return query_time, hits
   end
 end
 
--- spec/solr_performance_testing_spec.rb
+++ spec/solr_performance_testing_spec.rb
@@ -10,12 +10,33 @@ describe SolrPerformanceTesting do
   end
 
   it 'should benchmark' do
-    comp.search_companies(500, 5)
+    comp.search_companies(1, 1)
   end
 
   it 'should read lines' do
     lines = comp.companies.take(100)
     p lines
+  end
+
+  it 'convert query from log into rsolr query' do
+    a = 'facet=true&utf8=?&location=150-154-GB&shards.qt=dismax_flat&facet.limit=3&f.location.facet.limit=10&hl=true&version=2&fl=name,company_id,revenue,org_num,city,geolocation,geolocation_source,country_iso,industry_code,nace_2,naics_4,naics_6,employees_count,structure,profit,location,region,sub_region&bq=country_iso:GB^500+company_id:GB0000000288451024^9999&shards=localhost:8360/solr/no_companies_20130418_stem,localhost:8360/solr/gb_companies_20130602_stem,localhost:8360/solr/se_companies_20130423,localhost:8360/solr/dk_companies_20130423,localhost:8360/solr/global_20130423&geolocation={}&facet.query=employees_count:[1+TO+10]&facet.query=employees_count:[11+TO+50]&facet.query=employees_count:[51+TO+200]&facet.query=employees_count:[201+TO+500]&facet.query=employees_count:[501+TO+1000]&facet.query=employees_count:[1001+TO+5000]&facet.query=employees_count:[5001+TO+10000]&facet.query=employees_count:[10001+TO+10000000]&facet.query=revenue:[0.02+TO+500000]&facet.query=revenue:[0.05+TO+500000]&facet.query=revenue:[1+TO+500000]&facet.query=revenue:[10+TO+500000]&facet.query=revenue:[80+TO+500000]&facet.query=revenue:[200+TO+500000]&facet.query=revenue:[500+TO+500000]&facet.query=profit:[-2100+TO+-0.0001]&facet.query=profit:[0.02+TO+500000]&facet.query=profit:[0.05+TO+500000]&facet.query=profit:[1+TO+500000]&facet.query=profit:[10+TO+500000]&facet.query=profit:[80+TO+500000]&facet.query=profit:[200+TO+500000]&facet.query=profit:[500+TO+500000]&bf=log(max(1,boost_revenue))^200&action=show&facet.field=location&facet.field=region&facet.field=naics_6&qt=dismax_flat&fq=location:150-154-GB&fq=-active:1&fq=-region:039&fq=naics_6:551112+OR+name:"holding"&f.naics_6.facet.limit=30&hl.fragsize=280&industry_classification=naics_6&industry_coverage=0.6&facet.mincount=1&qf=body_1^0.05+body^0.1+body_2^0.01+chairman+structure^0.1+keywords^0.3+ext_link_texts^0.5+city^4+key_people+ceo+title^0.3+meta_description^0.3+name^10+board_member&resource=search&hl.fl=body&hl.fl=body_1&hl.fl=body_2&hl.fl=name&hl.fl=title&hl.fl=keywords&hl.fl=meta_description&hl.fl=ceo&hl.fl=chairman&hl.fl=board_member&hl.fl=key_people&hl.maxAnalyzedChars=500000&wt=javabin&f.region.facet.limit=5&rows=15&pf=title^10+meta_description^10+keywords^10+name^10&guess_facets=&start=0&q=holding+OR+company_id:GB0000000288451024&user_country_iso=GB&controller=companies/search&hl.usePhraseHighlighter=true'
+
+    b = a.split('&').map { |it| it.split('=').map { |p| "'#{p}'" } }
+
+    result = Hash.new { |hash, key| hash[key] = [] }
+
+    b.each do |a, b|
+      result[a] << b
+    end
+
+    result2 = {}
+    result.each do |k,v|
+      result2[k] = v.size > 1 ? "[#{v.join(',')}]" : v[0]
+    end
+
+    result2.each do |a,b|
+      puts "#{a} => #{b}, "
+    end
   end
 
 end
